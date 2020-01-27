a5f9813cde26c402a1f6bed55f1bc8baff22c120
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-01-27 22:01:40 +0000

    Accept code recommendation to quote arguments and show how timings in CTest output are unstable (and one line of build output too)

--- test/approvals/test.sh
+++ test/approvals/test.sh
@@ -6,8 +6,8 @@ script_dir='.'
 touch $script_dir/run_tests_after_build.received.txt  $script_dir/run_tests_after_build.approved.txt
 touch $script_dir/make_and_ctest.received.txt         $script_dir/make_and_ctest.approved.txt
 
-$script_dir/run_tests_after_build.py  $* > $script_dir/run_tests_after_build.received.txt   2>&1
-$script_dir//make_and_ctest.py        $* > $script_dir/make_and_ctest.received.txt          2>&1
+$script_dir/run_tests_after_build.py  "$@"  > $script_dir/run_tests_after_build.received.txt   2>&1
+$script_dir//make_and_ctest.py        "$@"  > $script_dir/make_and_ctest.received.txt          2>&1
 
 compare $script_dir/run_tests_after_build.received.txt  $script_dir/run_tests_after_build.approved.txt
 compare $script_dir/make_and_ctest.received.txt         $script_dir/make_and_ctest.approved.txt
